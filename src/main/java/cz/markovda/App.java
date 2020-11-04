bd01bfe84c1fde2a5bab6b2666988c7b3d3a8d2d
David Markov <markovd@students.zcu.cz>
2020-11-04 22:19:05 +0100

    Creating basic design.

--- src/main/java/cz/markovda/App.java
+++ src/main/java/cz/markovda/App.java
@@ -1,10 +1,14 @@
 package cz.markovda;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -14,12 +18,25 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private double xOffset;
+    private double yOffset;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"));
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
+        scene.setFill(Color.TRANSPARENT);
         stage.show();
+        moveScreen(stage);
+    }
+
+    public void exit(){
+
+    }
+
+    public void start() {
+
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -29,6 +46,23 @@ public class App extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+
+    private void moveScreen(Stage stage) {
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                xOffset = mouseEvent.getSceneX();
+                yOffset = mouseEvent.getSceneY();
+            }
+        });
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                stage.setX(mouseEvent.getScreenX()-xOffset);
+                stage.setY(mouseEvent.getScreenY()-yOffset);
+            }
+        });
     }
 
     public static void main(String[] args) {
